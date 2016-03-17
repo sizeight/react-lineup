2562d63a005b9007ce4c00713cc1727e530336b0
sizeight <hpretorius@gmail.com>
2016-03-17 12:36:55 +0200

    initial state

--- src/reducers/acts.js
+++ src/reducers/acts.js
@@ -1,4 +1,37 @@
-const initialState = {};
+const initialState = [
+  {
+        id: 1,
+        title: 'Queen',
+        stage: 'Main stage',
+        date: '2016/03/18',
+        begin: '12:00',
+        end: '13:00',
+    },
+    {
+        id: 2,
+        title: 'Metallica',
+        stage: 'Main stage',
+        date: '2016/03/18',
+        begin: '13:00',
+        end: '14:00',
+    },
+    {
+        id: 3,
+        title: 'The Prodigy',
+        stage: 'Electronic stage',
+        date: '2016/03/18',
+        begin: '12:00',
+        end: '13:00',
+    },
+    {
+        id: 4,
+        title: 'The Chemical Brothers',
+        stage: 'Electronic stage',
+        date: '2016/03/18',
+        begin: '13:00',
+        end: '14:00',
+    },
+];
 
 export default function acts(state = initialState, action) {
   switch (action.type) {
